a3c2f893e3991f88d3d914f91d3ec4049f7827ff
Liza <lizininua2001@gmail.com>
2023-08-09 18:20:11 +0400

    select company

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import {HistoryComponent} from "./history/history.component";
 import { AppComponent } from './app.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { CompanyRegistrationComponent } from './company-registration/company-registration.component';
+
 
 const routes: Routes = [
   {path: 'home', component: HistoryComponent},
@@ -13,7 +15,9 @@ const routes: Routes = [
   {path: 'history', component: HistoryComponent},
   {path: '', component: LoginFormComponent},
   {path: '', component: LoginFormComponent},
-  {path: 'registration', component: RegistrationFormComponent}
+  {path: 'registration', component: RegistrationFormComponent},
+  {path: 'company-registration', component: CompanyRegistrationComponent },
+
 ];
 
 @NgModule({
