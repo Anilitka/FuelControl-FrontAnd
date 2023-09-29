438d9c40a5f2a404109520cacfbf78bda1175d95
lizann29 <lizininua2001@gmail.com>
2023-09-29 16:36:11 +0400

    pagination not working

--- src/app/delete-tag-modal/delete-tag-modal.component.ts
+++ src/app/delete-tag-modal/delete-tag-modal.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FuelService } from '../services/fuel.service';
+import { TokenService } from '../services/token.service';
 
 @Component({
   selector: 'app-delete-tag-modal',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-tag-modal.component.css']
 })
 export class DeleteTagModalComponent {
+  constructor(
+    private fuelService: FuelService,
+    private http: HttpClient,
+    private tokenService: TokenService,
+  ) {}
+  allIdentifiedTags = [];
+  totalItems: any;
+  totalPages: any;
+  pageIndex = 1
 
+  ngOnInit(): void {
+   this.fillAllIdentifiedTags();
+  }
+
+  fillAllIdentifiedTags() {
+    const token = this.tokenService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  
+    this.http.get<any>(`https://localhost:5001/api/FuelTracking/GetAllIdentifiedTags?pageIndex=${this.pageIndex}`, { headers })
+      .subscribe(
+        (response) => {
+          console.log('Response:', response);
+           this.allIdentifiedTags = response;
+           console.log(this.allIdentifiedTags);
+        },
+        (error) => {
+          console.error('Error:', error);
+        }
+      );
+  }
+
+  previousPage(tag: any) {
+    if (tag.pageIndex > 1) {
+      tag.currentPage--;
+    }
+  }
+  
+  nextPage(tag: any) {
+    if (tag.currentPage < 10) {
+      tag.currentPage++;
+    }
+  }
+  
 }
